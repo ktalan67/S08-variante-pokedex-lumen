2d741fcad5f0756e259fcfb06aaf4ee5b05df56a
ktalan67 <56208640+ktalan67@users.noreply.github.com>
2020-01-19 16:29:19 +0100

    TEST allLinks Ok

--- resources/views/main/type.php
+++ resources/views/main/type.php
@@ -4,14 +4,12 @@
 
 <div class="row container-fluid d-flex justify-content-center">
     <p><?= $typeId->name ?></p>
-    <?php foreach ($typeId->getPokemonsByType as $pokemonId):?>
 
-        <div class="card col-3" style="width: 18rem;">
-            <div class="card-body">
-                <p class="card-text"><?= "#".$pokemonId->numero." ".$pokemonId->nom?></p></a>
-            </div><!---->
-        </div><!---->
 
-    <?php endforeach;?>
+
+
+
+
+    
 </div>
 </section>
--- resources/views/main/types.php
+++ resources/views/main/types.php
@@ -2,7 +2,7 @@
 <p>Cliquez sur un type pour voir tous les pokemon de ce type</p>
 <div class="listType">
 <?php foreach ($typeList as $type):?>
-<div class="titreType" style="background-color: #<?= $type->color?>"><a href="<?= route('type')?>" class="link"><?=$type->name?></a></div>
+<div class="titreType" style="background-color: #<?= $type->color?>"><a href="<?= route("type", ['id' => $type->id])?>" class="link"><?=$type->name?></a></div>
 <?php endforeach?>
 </div>
 
